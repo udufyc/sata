18373282fa6200421642f83d339966a5b06a8178
udufyc <109870941+udufyc@users.noreply.github.com>
2022-07-30 01:36:01 +0430

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,11 +54,11 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
+global.vcardowner = ['989175919318'] //ur owner number
+global.ownername = "SATA" //ur owner name
+global.ytname = "YT: Sata" //ur yt chanel name
 global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.location = "iran, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
 global.owner = ['989175919318']
